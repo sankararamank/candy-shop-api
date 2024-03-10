443e89af5bd903da55462c7bdf56a0fa06015aad
sankararamank <krishnan2098@gmail.com>
2024-03-10 05:55:32 +0530

    Minor environment improvements

--- config/settings.py
+++ config/settings.py
@@ -19,15 +19,15 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv("SECRETY_KEY"))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+ENVIRONMENT = str(os.getenv("ENVIRONMENT")).lower()
+# TODO: Find better way to manage dev and prod settings, else raise errors
+#  for wrong values of ENVIRONMENT
+DEBUG = True if  "dev" in ENVIRONMENT else False
 
 ALLOWED_HOSTS = []
 
